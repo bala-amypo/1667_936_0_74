df42f8a9e0ba8c77ca323f2153df9a5168226828
bala-amypo <bala@amypo.in>
2025-12-17 05:38:36 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,14 +2,14 @@ package com.example.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Beam;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
 public class SwaggerConfig{
 
-    @Beam
+    @Bean
     public OpenAPI customOpenAPI(){
         return new OpenAPI
             .servers(List.of(
