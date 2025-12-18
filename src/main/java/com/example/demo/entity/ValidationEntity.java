cc14b97de6d9e0f84efcb9f1da27a0228ad70115
bala-amypo <bala@amypo.in>
2025-12-18 08:18:47 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -27,4 +27,45 @@ public class ValidationEntity{
      @NotNull
      @Positive(message="Age should be a positive number")
      private int age
+
+    public void setId(Long id){
+        this.id = id;
+     }
+
+     public Long getId(){
+        return id;
+     }
+
+     public String getUsername(){
+        return username;
+     }
+
+     public void setUsername(String username){
+        this.username= username;
+     }
+     public String getEmail(){
+        return email;
+     }
+     
+     public void setEmail(String email){
+        this.email=email;
+     }
+
+     public String getPassword(){
+        return password;
+     }
+     
+     public void setPassword(String password){
+        this.password = password;
+     }
+
+     public int getAge(){
+     return age;
+     }
+
+     public void setAge(int age){
+     this.age= age;
+     }
+
+     public ValidationEntity(Long id,@NotNull @Size(min = 2 max
 }
