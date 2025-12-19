26ce775e9b26dd163669b07c3ff34972bbc92201
bala-amypo <bala@amypo.in>
2025-12-19 10:06:45 +0000

    User commit

--- src/main/java/com/example/demo/entity/Identity.java
+++ src/main/java/com/example/demo/entity/Identity.java
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Id{
+public class Identity{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
      private Integer id ;
