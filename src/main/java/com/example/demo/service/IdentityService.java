ee799b0b13e81033ec02d9c330dbca7c4b54c5a6
bala-amypo <bala@amypo.in>
2025-12-19 10:30:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/IdentityService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Identity;
+
+public interface IdentityService {
+        Identity toPost(Identity ids);
+}
---
+++ src/main/java/com/example/demo/service/Impl/IdentityServiceImpl.java
@@ -0,0 +1,17 @@
+package com.example.demo.service.Impl;
+
+import com.example.demo.service.IdentityService;
+import com.example.demo.entity.Identity;
+import com.example.demo.repository.IdentityRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IdentityServiceImpl implements IdentityService{
+    @Autowired IdentityRepository ident;
+    @Override
+    public Identity toPost(Student stu){
+       return ident.save(stu);
+    }
+}
