56a153ab24336d518877a154160cc2a994c61cca
bala-amypo <bala@amypo.in>
2025-12-19 08:51:44 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimeStamp.java
+++ src/main/java/com/example/demo/entity/TimeStamp.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 
 import java.time.LocalDateTime;
 
@@ -25,6 +26,6 @@ public class TimeStamp{
 
     @PrePersist
     public void onCreate(){
-        
+         
     }
 }
